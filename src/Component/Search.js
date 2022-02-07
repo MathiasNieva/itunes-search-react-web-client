9c7f3adf915be390dde2687427bb65396cc2ddd7
Mathias Nieva Montero <mathiasnievamontero@gmail.com>
2022-02-07 16:43:41 -0400

    Added functionality to get songs from iTunes API

--- src/Component/Search.js
+++ src/Component/Search.js
@@ -1,8 +1,42 @@
 import { Container, Form } from 'react-bootstrap';
 import { useState, useEffect } from 'react';
+import axios from 'axios'
+
+const iTunesURL = "https://itunes.apple.com/search?"
 
 export default function Searchbar() {
     const [search, setSearch] = useState("")
+    const [searchResults, setSearchResults] = useState([])
+    console.log(searchResults)
+
+    useEffect(() =>{
+        if (!search) return setSearchResults([])
+        let cancel = false
+
+        axios.get(`${iTunesURL}term=${search}&limit=25&media=music&entity=song`).then(res => {
+            if (cancel) return
+
+            setSearchResults(res.data.results.map(item => {
+               let minutes = Math.floor((item.trackTimeMillis/1000)/60)
+               let seconds = Math.floor((item.trackTimeMillis/1000)%60)
+               seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+               return {
+                   artist: item.artistName,
+                   song: item.trackName,
+                   album: item.collectionName,
+                   art: item.artworkUrl100,
+                   id: item.trackId,
+                   preview: item.previewUrl,
+                   songPrice: item.trackPrice,
+                   currency: item.currency,
+                   songDuration: minutes + ":" + seconds
+               }
+           }))
+        }).catch(err => {console.log(err)})
+
+        return () => cancel = true
+    }, [search])
 
     return(
         <Container className="d-flex flex-column py-2 px-5" >
