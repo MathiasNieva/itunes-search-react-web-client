7f05fcfcc6ac168cf5cfdc443f69653a3d7cab72
Mathias Nieva Montero <mathiasnievamontero@gmail.com>
2022-02-08 15:34:40 -0400

    Added price and changes to positions and styles

--- src/Component/SearchResult.js
+++ src/Component/SearchResult.js
@@ -1,21 +1,25 @@
 import React from "react";
+import Badge from 'react-bootstrap/Badge'
 
 export default function SearchResult( {item} ) {
-    return <div className="d-flex m-2 p-2 align-items-center" style={{ border: '2px solid white'}}>
+    return <div className="d-flex m-2 p-2 align-items-center" style={{ border: '2px solid white', borderRadius: "10px"}}>
         <img src={item.art} alt="No image"></img>
         <div className="ms-3 w-50">
             <div>SONG: {item.song}</div> 
             <div>ARTIST: {item.artist}</div>
             <div>ALBUM: {item.album}</div>
+            <div>DURATION: {item.songDuration}</div>
         </div>
-        <div className="ms-3">
-            <div>Price: {item.songPrice} {item.currency}</div> 
-            <div>Duration: {item.songDuration}</div>
+        <div className="ms-1">
+            <h5 className="text-center">Preview:</h5>
         </div>
-        <div className="d-flex ms-3">
+        <div className="ms-2">
             <audio controls>
                 <source src={item.preview}></source>
             </audio>    
-        </div>     
+        </div>
+        <h3 className="ms-4">
+            <Badge pill bg="info" className="ms-1">{item.songPrice} {item.currency}</Badge>
+        </h3>      
     </div>
 }
